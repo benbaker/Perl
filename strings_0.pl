6ebce27002246c9e3659b23fffcc5d7eb152c463
Ben Baker <bensemailaddress@icloud.com>
2013-12-10 15:13:27 -0600

    Update strings_0.pl

--- strings_0.pl
+++ strings_0.pl
@@ -2,16 +2,14 @@
 use strict;
 use warnings;
 
-$passage = "I want to take this opportunity to welcome you to Mizzou for Spring Semester 2013.  For some of you, this will constitute a new beginning, For others, it will be one of many new starts on decades of growth, new experiences for education and service, and new friendships and career opportunities during decades of dedicated service.";
+$passage = "I want to take this opportunity to welcome you. ";
 
 my @words = split(/\s+/, $line);
-
 
 my $i=0;
 
 foreach my $word (@words) {
-
   $word =~s/[[:punct:]]//g;
   print $word . $i++;
-
 }
+
